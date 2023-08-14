e35c1538b55bb9265fd1eff971e690a3e09bd579
fukken <fukuda.k@gmail.com>
2023-08-14 19:00:49 +0900

    disable preroll

--- cast/js/receiver.js
+++ cast/js/receiver.js
@@ -14,12 +14,14 @@ playerManager.setMessageInterceptor(
 const breakClipUrl = 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4';
 const addVASTBreaksToMedia = (mediaInformation) => {
     mediaInformation.breakClips = [
+        /**
         {
             id: "bc1",
             title: "bc1 (Pre-roll)",
             contentUrl: breakClipUrl,
             whenSkippable: 5
         },
+        **/
         {
             id: "bc2",
             title: "bc2 (Mid-roll)",
